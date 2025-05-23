f744ae9e19989fbd79e15729a3773197d5131fd8
Ron Shabi <ron@ronsh.net>
2025-05-23 21:47:23 +0300

    server: use log_debug instead of log_info
    Signed-off-by: Ron Shabi <ron@ronsh.net>

--- server.c
+++ server.c
@@ -1,19 +1,22 @@
 #include "server.h"
 
-#include <sys/socket.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/socket.h>
 
 #include "logging.h"
 
-int server_init(struct server *s) {
+int server_init(struct server *s)
+{
+    log_debug("calling socket()");
     s->socket_fd = socket(PF_INET, SOCK_STREAM, 0);
 
-    if (s->socket_fd == -1) {
-        log_error("Can't create IPv4 socket (errno: %s)", strerror(errno));
+    if (s->socket_fd == -1)
+    {
+        log_error("can't create IPv4 socket (errno: %s)", strerror(errno));
         return 1;
     }
 
-    log_info("Created IPv4 socket (fd: %d)", s->socket_fd);
+    log_debug("created IPv4 socket (fd: %d)", s->socket_fd);
     return 0;
 }
