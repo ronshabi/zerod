fb7265ba5d4725290cc03b6c5fc17da366b855a4
Ron Shabi <ron@ronsh.net>
2025-05-25 23:14:12 +0300

    buffer: add new buffer (dynamic array) data structure
    Signed-off-by: Ron Shabi <ron@ronsh.net>

---
+++ buffer.c
@@ -0,0 +1,61 @@
+#include "buffer.h"
+#include "panic.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+void buffer_init(struct buffer *buf, uint64_t stride)
+{
+    memset(buf, 0, sizeof(struct buffer));
+    buf->stride = stride;
+}
+
+void buffer_clear(struct buffer *buf)
+{
+    if (buf->data)
+    {
+        memset(buf->data, 0, buf->cap * buf->stride);
+    }
+}
+
+void *buffer_push(struct buffer *buf, const void *elem)
+{
+    if (buf->len == buf->cap)
+    {
+        if (buf->data == NULL)
+        {
+            buf->data = malloc(buf->stride);
+            if (buf->data == NULL)
+            {
+                panic("buffer malloc failed");
+            }
+
+            buf->len = 0;
+            buf->cap = 1;
+        }
+        else
+        {
+            buf->data = realloc(buf->data, buf->cap * 2);
+            if (buf->data == NULL)
+            {
+                panic("buffer realloc failed");
+            }
+
+            buf->cap *= 2;
+        }
+    }
+
+    memcpy(buf->data + buf->len * buf->stride, elem, buf->stride);
+    ++buf->len;
+    return buf->data + (buf->len - 1) * buf->stride;
+}
+
+void *buffer_get_item_ptr(struct buffer *buf, uint64_t index)
+{
+    if (index >= buf->len)
+    {
+        return NULL;
+    }
+
+    return buf->data + index * buf->stride;
+}
---
+++ buffer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdint.h>
+
+struct buffer
+{
+    uint64_t len;
+    uint64_t cap;
+    uint64_t stride;
+    void    *data;
+};
+
+void buffer_init(struct buffer *buf, uint64_t stride);
+
+void buffer_ensure_capacity(struct buffer *buf, uint64_t wanted_cap);
+
+void buffer_clear(struct buffer *buf);
+
+void *buffer_push(struct buffer *buf, const void *elem);
+
+void *buffer_get_item_ptr(struct buffer *buf, uint64_t index);
