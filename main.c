a24cba3bbfab95312ce258be61f0cc9186b47457
Ron Shabi <ron@ronsh.net>
2025-05-26 21:39:17 +0300

    main: display PID when starting
    Signed-off-by: Ron Shabi <ron@ronsh.net>

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 #include "logging.h"
 #include "server.h"
 
+#include <unistd.h>
+
 int main(void)
 {
     struct server s;
 
-    log_info("Starting zerod");
+    log_info("Starting zerod (pid: %d)", getpid());
 
     server_init(&s, "8080", AF_INET);
 
